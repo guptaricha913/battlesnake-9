4f08b5999398e850cdfccbac1140b19535fd0a3a
XintongLi1 <1720731919@qq.com>
2021-04-03 22:24:34 -0400

    corrected a bug

--- preprocessing.py
+++ preprocessing.py
@@ -45,8 +45,8 @@ class Preprocessing:
 
     def neighbors(self, y, x):
         coordinates = {
-            'up': {'x': 0, 'y': -1},
-            'down': {'x': 0, 'y': 1},
+            'up': {'x': 0, 'y': 1},
+            'down': {'x': 0, 'y': -1},
             'left': {'x': -1, 'y': 0},
             'right': {'x': 1, 'y': 0},
         }
